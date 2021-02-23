a73b1aaa8fd9aaa973891d3597b2ac9f6f27c35b
Neengash <pepmanas93@gmail.com>
2021-02-23 11:04:59 +0100

    Test Scene working with Gyro

---
+++ Assets/UnityRemote/Gyro.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+In this version of the gyro controller we face a problem when
+working with euler angles. Even though the Y and Z axis work
+as one would expect, the X axis shows an unexpected behaviour
+in which the movement is restricted to a 180º arc, instead
+of the full circumference (like the other two).
+ */
+
+public class Gyro : MonoBehaviour
+{
+    public Vector3 gyro;
+    public Vector3 reference;
+    public bool referenced = false;
+    Quaternion myRotation;
+
+    void Start() {
+        Input.gyro.enabled = true;
+    }
+
+    void Update() {
+        if (Input.touchCount > 0) {
+            UpdateReference();
+        }
+
+        if (referenced) {
+            gyro = Input.gyro.attitude.eulerAngles;
+            transform.rotation = Quaternion.Euler(new Vector3(
+                GetAngleDifference(gyro.y, reference.y),
+                -1 * GetAngleDifference(gyro.x, reference.x),
+                GetAngleDifference(gyro.z, reference.z)
+            ));
+        }
+    }
+
+    private float GetAngleDifference(float gyro, float reference) {
+        return gyro - reference;
+    }
+
+    public void UpdateReference () {
+        reference = Input.gyro.attitude.eulerAngles;
+        referenced = true;
+    }
+
+}
---
+++ Assets/UnityRemote/Rotations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+This version of the gyro controll logic seems to do the trick
+but it has a problem, due to it working with rotate (so difference
+in rotations of the gyro) it looses tiny bits of information
+through the diferent iterations. This results in a situation where
+having the mobile phone in the same position at two different
+moments, doesn't keep the same camera orientation in the game.
+ */
+
+public class rotateY : MonoBehaviour
+{
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        Input.gyro.updateInterval = 0.02f;
+    }
+
+    private void FixedUpdate() {
+        transform.Rotate (
+            -Input.gyro.rotationRateUnbiased.x,
+            -Input.gyro.rotationRateUnbiased.y,
+            Input.gyro.rotationRateUnbiased.z);
+    }
+}
