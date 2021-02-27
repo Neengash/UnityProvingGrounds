8c674cb668ae81f958b067257bf31f9fc3603e24
Neengash <pepmanas93@gmail.com>
2021-02-27 12:27:56 +0100

    Accelerometer Movement Testing - with acceleration

---
+++ Assets/AccelerometerMovement2D/AccelerationMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerationMovement : MonoBehaviour
+{
+    private Vector3 acceleration;
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float forceMultiplication;
+    private float previousDirection;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        acceleration = Input.acceleration;
+        // Si acaba de canviar sa direcció, reset de les velocitats;
+        if (previousDirection * acceleration.x < 0) {
+            rb.velocity = new Vector3();
+        }
+        rb.AddForce(new Vector3(acceleration.x * forceMultiplication, 0, 0));
+        previousDirection = acceleration.x;
+    }
+
+}
