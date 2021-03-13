7ac0813a8eecce9b0ffc3758fb67cdf042d553d0
Neengash <pepmanas93@gmail.com>
2021-03-13 14:11:34 +0100

    add randomTests Scene

---
+++ Assets/Random/SpaceActivator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceActivator : MonoBehaviour
+{
+    public GameObject foo;
+
+    void Start() {
+        Debug.Log("Started");
+        int a = 1 + 1;
+        Debug.Log(a);
+        doSomething();
+    }
+
+    void Update() {
+        foo.SetActive(!Input.GetKey(KeyCode.Space));
+    }
+
+    void doSomething() {
+        doSomethingAgain();
+    }
+
+    void doSomethingAgain() {
+        int i = 0;
+        int j = 1;
+        Debug.Log(i+j);
+    }
+}
